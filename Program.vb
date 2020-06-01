e145e0c8dcfdb39b98c9f9f7625dc6b3fec9b543
VbScrub <53273102+VbScrub@users.noreply.github.com>
2020-06-01 23:35:01 +0100

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.ComponentModel
+
+Module Program
+
+    Sub Main()
+
+        If My.Application.CommandLineArgs.Count < 2 Then
+            Console.WriteLine("Usage:" & Environment.NewLine &
+                              "ServiceInstallTest.exe ServiceName ExePath")
+            Exit Sub
+        End If
+        ' Get command line args
+        Dim ServiceName As String = My.Application.CommandLineArgs(0)
+        Dim ExePath As String = My.Application.CommandLineArgs(1)
+        Dim ScmHandle As IntPtr = IntPtr.Zero
+        Dim ServiceHandle As IntPtr = IntPtr.Zero
+
+        Try
+            ' Connect to SCM (Service Control Manager) and get a handle that allows us to create services by specifying SC_MANAGER_CREATE_SERVICE. 
+            ' Would fail with access denied here if we don't have permission to create services)
+            ScmHandle = WinApi.OpenSCManager(Nothing, Nothing, WinApi.SCM_RIGHTS.SC_MANAGER_CREATE_SERVICE)
+            If ScmHandle = IntPtr.Zero Then
+                Throw New Win32Exception()
+            End If
+
+            Try
+                ' Create a new service and get back a handle to the service (that can then be used with StartService function and other service functions). 
+                ' We specify SERVICE_ALL_ACCESS so that this handle allows us full control over the service (to start it, stop it, delete it, etc) and for some
+                ' reason this works. If we try to request this level of access afterwards by using the OpenService function, it fails with access denied
+                ServiceHandle = WinApi.CreateService(ScmHandle,
+                                                    ServiceName,
+                                                    Nothing,
+                                                    WinApi.SERVICE_RIGHTS.SERVICE_ALL_ACCESS,
+                                                    WinApi.SERVICE_WIN32_OWN_PROCESS,
+                                                    WinApi.SERVICE_DEMAND_START,
+                                                    WinApi.SERVICE_ERROR_NORMAL,
+                                                    ExePath,
+                                                    Nothing, Nothing, Nothing, "NT Authority\System", Nothing)
+
+                ' If we successfully created the service, attempt to start it using the handle we got back from CreateService. 
+                If Not ServiceHandle = IntPtr.Zero Then
+                    If WinApi.StartService(ServiceHandle, 0, IntPtr.Zero) Then
+                        Console.Write("Service created and started successfully")
+                    Else
+                        Throw New Exception("Service has been created but was unable to start due to error: " & New System.ComponentModel.Win32Exception().Message)
+                    End If
+                Else
+                    Throw New Exception("Unable to create service due to error: " & New System.ComponentModel.Win32Exception().Message)
+                End If
+
+            Finally
+                If Not ServiceHandle = IntPtr.Zero Then
+                    WinApi.CloseServiceHandle(ServiceHandle)
+                End If
+            End Try
+        Catch ex As Exception
+            Console.WriteLine("Error: " & ex.Message)
+        Finally
+            If Not ScmHandle = IntPtr.Zero Then
+                WinApi.CloseServiceHandle(ScmHandle)
+            End If
+        End Try
+    End Sub
+
+
+End Module
---
+++ WinApi.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class WinApi
+
+    ' Everything needed to allow us to call the relevant Win32 APIs from .NET code.
+    ' See links to native API definitions on each item for further details
+
+    Public Const SERVICE_WIN32_OWN_PROCESS As UInteger = &H10
+    Public Const SERVICE_ERROR_NORMAL As UInteger = 1
+    Public Const SERVICE_AUTO_START As UInteger = 2
+    Public Const SERVICE_DEMAND_START As UInteger = 3
+    Public Const SERVICE_DISABLED As UInteger = 4
+
+    'https://docs.microsoft.com/en-us/windows/win32/services/service-security-and-access-rights#access-rights-for-a-service
+    <Flags()> _
+    Public Enum SERVICE_RIGHTS As UInteger
+        SERVICE_QUERY_CONFIG = 1
+        SERVICE_CHANGE_CONFIG = 2
+        SERVICE_QUERY_STATUS = 4
+        SERVICE_ENUMERATE_DEPENDENTS = 8
+        SERVICE_START = &H10
+        SERVICE_STOP = &H20
+        SERVICE_INTERROGATE = &H80
+        SERVICE_USER_DEFINED_CONTROL = &H100
+        SERVICE_PAUSE_CONTINUE = &H40
+        SERVICE_ALL_ACCESS = &HF01FF
+        ACCESS_SYSTEM_SECURITY = &H1000000
+        DELETE = &H10000
+        WRITE_DAC = &H40000
+        WRITE_OWNER = &H80000
+        SYNCHRONIZE = &H100000
+    End Enum
+
+    'https://docs.microsoft.com/en-us/windows/win32/services/service-security-and-access-rights
+    <Flags()> _
+    Public Enum SCM_RIGHTS As UInteger
+        SC_MANAGER_CONNECT = 1
+        SC_MANAGER_CREATE_SERVICE = 2
+        SC_MANAGER_ENUMERATE_SERVICE = 4
+        SC_MANAGER_LOCK = 8
+        SC_MANAGER_MODIFY_BOOT_CONFIG = &H20
+        SC_MANAGER_QUERY_LOCK_STATUS = &H10
+        SC_MANAGER_ALL_ACCESS = &HF003F
+        DELETE = &H10000
+        WRITE_DAC = &H40000
+        WRITE_OWNER = &H80000
+        SYNCHRONIZE = &H100000
+    End Enum
+
+    'https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-startservicew
+    <DllImport("advapi32.dll", EntryPoint:="StartServiceW", SetLastError:=True)> _
+    Public Shared Function StartService(ByVal hService As IntPtr, ByVal dwNumServiceArgs As UInteger, _
+                                        ByVal lpServiceArgVectors As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    'https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-createservicew
+    <DllImport("advapi32.dll", EntryPoint:="CreateServiceW", SetLastError:=True)> _
+    Public Shared Function CreateService(ByVal hSCManager As IntPtr, <MarshalAs(UnmanagedType.LPWStr)> ByVal lpServiceName As String, _
+                                         <MarshalAs(UnmanagedType.LPWStr)> ByVal lpDisplayName As String, ByVal dwDesiredAccess As UInteger, ByVal dwServiceType As UInteger, ByVal dwStartType As UInteger,
+                                         ByVal dwErrorControl As UInteger, <MarshalAs(UnmanagedType.LPWStr)> ByVal lpBinaryPathName As String, <MarshalAs(UnmanagedType.LPWStr)> ByVal lpLoadOrderGroup As String, _
+                                         ByVal lpdwTagId As IntPtr, <MarshalAs(UnmanagedType.LPWStr)> ByVal lpDependencies As String, <MarshalAs(UnmanagedType.LPWStr)> ByVal lpServiceStartName As String, _
+                                         <MarshalAs(UnmanagedType.LPWStr)> ByVal lpPassword As String) As IntPtr
+    End Function
+
+    'https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-openscmanagerw
+    <DllImport("advapi32.dll", EntryPoint:="OpenSCManagerW", SetLastError:=True)> _
+    Public Shared Function OpenSCManager(<MarshalAs(UnmanagedType.LPWStr)> ByVal lpMachineName As String, _
+                                         <MarshalAs(UnmanagedType.LPWStr)> ByVal lpDatabaseName As String, _
+                                         ByVal dwDesiredAccess As UInteger) As IntPtr
+    End Function
+
+    'https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-closeservicehandle
+    <DllImport("advapi32.dll", EntryPoint:="CloseServiceHandle", SetLastError:=True)> _
+    Public Shared Function CloseServiceHandle(ByVal hSCObject As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    <DllImport("advapi32.dll", EntryPoint:="OpenServiceW", SetLastError:=True)> _
+    Public Shared Function OpenService(ByVal hSCManager As IntPtr, <MarshalAs(UnmanagedType.LPWStr)> ByVal lpServiceName As String, _
+                                        ByVal dwDesiredAccess As UInteger) As IntPtr
+    End Function
+
+End Class
